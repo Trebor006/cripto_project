6702f56a529babbb7dd7b2c7b4d31c9a8d4f9988
Robert Cabrera <clrobert.06@gmail.com>
2023-05-15 21:14:51 -0400

    feat(master): Cifrado Cesar

--- encryptors/desplazamiento/puro.go
+++ encryptors/desplazamiento/puro.go
@@ -1,18 +1,46 @@
 package desplazamiento
 
-import "criptograms/main/data"
+import (
+	"criptograms/main/data"
+	util "criptograms/main/util"
+	"strings"
+)
 
 type Puro struct {
 }
 
 func (r Puro) Cypher(data data.Data) string {
-	//todo implementar aqui la logica para cifrar!!!
+	message := strings.ToUpper(strings.ReplaceAll(data.Message, " ", ""))
+	alfabeto := util.ObtenerAlfabetoEspanol()
 
-	return "Puro Encripted message : " + data.Message
+	textoEncriptado := ""
+	for _, char := range message {
+		pos := util.ObtenerPosicion(alfabeto, string(char))
+		nuevaPos := pos + 3
+		if nuevaPos >= len(alfabeto) {
+			nuevaPos = nuevaPos - len(alfabeto)
+		}
+
+		textoEncriptado += alfabeto[nuevaPos]
+	}
+
+	return textoEncriptado
 }
 
 func (r Puro) Decrypt(data data.Data) string {
-	//todo implementar aqui la logica para descifrar!!!
+	message := strings.ToUpper(strings.ReplaceAll(data.EncryptedMessage, " ", ""))
+	alfabeto := util.ObtenerAlfabetoEspanol()
 
-	return "Puro Encripted message : " + data.Message
+	textoEncriptado := ""
+	for _, char := range message {
+		pos := util.ObtenerPosicion(alfabeto, string(char))
+		nuevaPos := pos - 3
+		if nuevaPos < 0 {
+			nuevaPos = len(alfabeto) + nuevaPos
+		}
+
+		textoEncriptado += alfabeto[nuevaPos]
+	}
+
+	return textoEncriptado
 }
